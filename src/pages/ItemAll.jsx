619ae81b46f384303dfb96e20800abf68511b732
dante0214 <rn_chic@naver.com>
2024-08-21 16:05:42 +0900

    feat: api 에러 핸들링

--- src/pages/ItemAll.jsx
+++ src/pages/ItemAll.jsx
@@ -1,30 +1,46 @@
 import React, { useEffect, useState } from "react";
 import ItemCard from "../components/ItemCard";
-import { Container, Grid } from "@mui/material";
+import { CircularProgress, Container, Grid } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 
 const ItemAll = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const [itemList, setItemList] = useState([]);
   const [query, setQuery] = useSearchParams();
   const getItems = async () => {
-    const searchQuery = query.get("q") || "";
-    const url = `https://my-json-server.typicode.com/Dante0214/shoppingmall-js/products?q=${searchQuery}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    setItemList(data);
+    setLoading(true);
+    try {
+      const searchQuery = query.get("q") || "";
+      const url = `https://my-json-server.typicode.com/Dante0214/shoppingmall-js/products?q=${searchQuery}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setItemList(data);
+    } catch (error) {
+      setError("불러오기 실패");
+    } finally {
+      setLoading(false);
+    }
   };
   useEffect(() => {
     getItems();
   }, [query]);
+  if (loading || itemList == null) return <CircularProgress />;
   return (
     <Container>
-      <Grid container spacing={2}>
-        {itemList.map((item) => (
-          <Grid item md={3} sm={12} key={item.id}>
-            <ItemCard item={item} />
-          </Grid>
-        ))}
-      </Grid>
+      {error ? (
+        <Alert severity="error" className="text-center">
+          {error}
+        </Alert>
+      ) : (
+        <Grid container spacing={2}>
+          {itemList.map((item) => (
+            <Grid item md={3} sm={12} key={item.id}>
+              <ItemCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Container>
   );
 };
--- src/pages/ItemDetail.jsx
+++ src/pages/ItemDetail.jsx
@@ -1,10 +1,10 @@
 import {
   Button,
+  CircularProgress,
   Container,
   FormControl,
   Grid,
   InputLabel,
-  Menu,
   MenuItem,
   Select,
   Typography,
@@ -16,6 +16,8 @@ const ItemDetail = () => {
   const { id } = useParams();
   const [item, setItem] = useState(null);
   const [selectedSize, setSelectedSize] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     setSelectedSize(e.target.value);
@@ -25,68 +27,78 @@ const ItemDetail = () => {
     setSelectedSize("");
   };
   const getItem = async () => {
-    const url = `https://my-json-server.typicode.com/Dante0214/shoppingmall-js/products/${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    setItem(data);
-    console.log(item);
+    setLoading(true);
+    try {
+      const url = `https://my-json-server.typicode.com/Dante0214/shoppingmall-js/products/${id}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setItem(data);
+    } catch (error) {
+      setError("불러오기 실패");
+    } finally {
+      setLoading(false);
+    }
   };
   useEffect(() => {
     getItem();
   }, [id]);
 
-  if (!item) {
-    return <Typography variant="h6">Loading...</Typography>;
-  }
+  if (loading || item == null) return <CircularProgress />;
 
   return (
     <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <img
-            src={item.img}
-            alt={item.title}
-            style={{ width: "100%", height: "80%" }}
-          />
+      {error ? (
+        <Alert severity="error" className="text-center">
+          {error}
+        </Alert>
+      ) : (
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <img
+              src={item.img}
+              alt={item.title}
+              style={{ width: "100%", height: "80%" }}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {item.title}
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              ₩ {item.price}
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {item.choice ? "Conscious choice" : ""}
+            </Typography>
+            <form onSubmit={handleSubmit}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="select-size-label">사이즈 선택</InputLabel>
+                <Select
+                  labelId="select-size-label"
+                  value={selectedSize}
+                  onChange={handleChange}
+                  label="사이즈 선택"
+                >
+                  {item.size &&
+                    item.size.map((size) => (
+                      <MenuItem key={size} value={size}>
+                        {size}
+                      </MenuItem>
+                    ))}
+                </Select>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  sx={{ mt: 2 }}
+                >
+                  담기
+                </Button>
+              </FormControl>
+            </form>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h5" gutterBottom>
-            {item.title}
-          </Typography>
-          <Typography variant="h6" gutterBottom>
-            ₩ {item.price}
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {item.choice ? "Conscious choice" : ""}
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            <FormControl fullWidth variant="outlined">
-              <InputLabel id="select-size-label">사이즈 선택</InputLabel>
-              <Select
-                labelId="select-size-label"
-                value={selectedSize}
-                onChange={handleChange}
-                label="사이즈 선택"
-              >
-                {item.size &&
-                  item.size.map((size) => (
-                    <MenuItem key={size} value={size}>
-                      {size}
-                    </MenuItem>
-                  ))}
-              </Select>
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                sx={{ mt: 2 }}
-              >
-                담기
-              </Button>
-            </FormControl>
-          </form>
-        </Grid>
-      </Grid>
+      )}
     </Container>
   );
 };
